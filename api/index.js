f311d46e9438de3522ba2614218cfc14d44a1b1a
PVSKR <skr.polu@hotmail.com>
2024-05-03 16:01:01 +0530

    app.get()method implemented to  check that  work or not   in url
    url:http://localhost:5038/api/todoapp/GetNotes.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 var Express = require("express");
-var MongoClient = require("mongodb").MongoClient;
+var Mongoclient = require("mongodb").MongoClient;
 var cors = require("cors");
 const multer = require("multer");
 var app = Express();
@@ -7,15 +7,24 @@ app.use(cors());
 var CONNECTION_STRING =
   "mongodb+srv://admin:Yrskrmsr@cluster0.nwljkhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-var DATABASE_NAME = "todoappdb";
+var DATABASENAME = "todoappdb";
 var database;
 app.listen(5038, () => {
-  MongoClient.connect(CONNECTION_STRING, (error, client) => {
+  Mongoclient.connect(CONNECTION_STRING, (error, client) => {
     if (error) {
       console.error("Error connecting to MongoDB:", error);
       return;
     }
-    database = client.db(DATABASE_NAME);
+    database = client.db(DATABASENAME);
     console.log("MongoDB Connection Successful");
   });
+});
+
+app.get("/api/todoapp/GetNotes", (request, response) => {
+  database
+    .collection("todoappcollect")
+    .find({})
+    .toArray((error, result) => {
+      response.send(result);
+    });
 });
