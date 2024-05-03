807b0ea30387088c8286feb619472f65118cf044
PVSKR <skr.polu@hotmail.com>
2024-05-03 12:47:57 +0530

    client side api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+var Express = require("express");
+var MongoClient = require("mongodb").MongoClient;
+var cors = require("cors");
+const multer = require("multer");
+var app = Express();
+app.use(cors());
+var CONNECTION_STRING =
+  "mongodb+srv://SravanKumarPolu:Yrskrmsr@1@cluster0.nwljkhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+var DATABASE_NAME = "todoappdb";
+var database;
+app.listen(5038, () => {
+  MongoClient.connect(CONNECTION_STRING, (error, client) => {
+    if (error) {
+      console.error("Error connecting to MongoDB:", error);
+      return;
+    }
+    database = client.db(DATABASE_NAME);
+    console.log("MongoDB Connection Successful");
+  });
+});
