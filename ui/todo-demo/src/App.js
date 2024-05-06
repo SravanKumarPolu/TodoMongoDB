7641090d6d37263d35c59a54a321795023d63de7
PVSKR <skr.polu@hotmail.com>
2024-05-06 12:01:39 +0530

    style of  todo modified.

--- ui/todo-demo/src/App.js
+++ ui/todo-demo/src/App.js
@@ -44,21 +44,39 @@ function App() {
     }
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ToDo App</h1>
-        <input
-          id="newNotes"
-          value={newNote}
-          onChange={(e) => setNewNote(e.target.value)}
-        />
-        <button onClick={addClick}>Add Note</button>
-        {notes.map((note, index) => (
-          <p key={index}>
-            <b>*{note.description}</b>&nbsp;
-            <button onClick={() => deleteClick(note.id)}>Delete</button>
-          </p>
-        ))}
+    <div className=" bg-slate-500 min-h-screen pt-10 md:pt-20 lg:pt-32 xl:pt-40">
+      <header className="flex flex-col gap-5 text-center text-yellow-50">
+        <h1 className="   font-semibold text-2xl">ToDo App</h1>
+        <div className=" flex flex-row justify-center gap-4 align-middle">
+          <input
+            className=" w-[20rem] text-black xs:w-full px-2 sm:w-full md:w-[20rem] h-[2.5rem] rounded-sm shadow-md"
+            id="newNotes"
+            value={newNote}
+            onChange={(e) => setNewNote(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600 shadow-sm"
+            onClick={addClick}>
+            Add Note
+          </button>
+        </div>
+        <div className=" flex w-full flex-col gap-4 justify-center items-center">
+          {notes.map((note, index) => (
+            <p key={index} className="">
+              <span className="flex flex-row px-10 items-center justify-center  text-center  gap-5">
+                <b className=" flex-wrap text-left  w-[20rem] ">
+                  {note.description}
+                </b>
+                &nbsp;
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:bg-red-600  shadow-sm"
+                  onClick={() => deleteClick(note.id)}>
+                  Delete
+                </button>
+              </span>
+            </p>
+          ))}
+        </div>
       </header>
     </div>
   );
